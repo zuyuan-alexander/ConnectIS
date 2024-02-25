7b668c245bd27351ca6681833bacb7cc6d58679f
zuyuan-alexander <e0985949@u.nus.edu>
2024-02-25 17:27:52 +0800

    Update on UserSessionBean

---
+++ OneDrive/Desktop/NUS/Y2S2/IS3106/ConnectIS/ConnectIS-ejb/src/java/session/UserSessionBean.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package session;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author zuyua
+ */
+@Stateless
+public class UserSessionBean implements UserSessionBeanLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
